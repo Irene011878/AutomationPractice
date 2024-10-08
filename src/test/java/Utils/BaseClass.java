3161b2078537b17cec034d9dd5988133ffd34c26
Irene011878 <IA00833195@TechMahindra.com>
2024-10-08 17:30:13 -0600

    Fix Adding headlees

--- src/test/java/Utils/BaseClass.java
+++ src/test/java/Utils/BaseClass.java
@@ -4,6 +4,7 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -32,7 +33,10 @@ public class BaseClass {
     @BeforeMethod(groups = {"regression","smoke"})
                       //4 oct
     public void setUp(Method method){
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--windows-size=1920, 1080");
+        driver = new ChromeDriver(options);
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
